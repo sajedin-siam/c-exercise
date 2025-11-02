3fb090bccb738bc54fabdfba794929babd686a7e
sajedin-siam <sajedinsiam73@gmail.com>
2025-11-02 22:54:17 +0600

    Initial commit

---
+++ one.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Sajedin Siam");
+    return 0;
+}
---
+++ ten.c
@@ -0,0 +1,10 @@
+//write a program that read any character and disply ASCII value.
+#include<stdio.h>
+int main()
+{
+    char n;
+    printf("Enter any character:");  
+    scanf("%c", &n);
+    printf("ASCII value is: %d", n);  
+    return 0;
+}
