4cd0fa41a4b88c974c533ff76d8e883000e93d46
Sajedin Siam <sajedinsiam73@gmail.com>
2026-01-25 19:09:43 +0600

    Binary element searching

--- Binary_search.c
+++ Binary_search.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int binarysearch(int arr[], int n, int search)
 {
-    int low=0, high = n - 1;
+    int low = 0, high = n - 1;
     while (low <= high)
     {
         int mid = (low + high) / 2;
         if (arr[mid] == search)
         {
-            return mid;//element found mid
+            return mid; // element found mid
         }
         else if (arr[mid] < search)
         {
-            low= mid + 1; // element searching right side
+            low=mid + 1; // element searching right side
         }
         else
         {
@@ -22,7 +22,7 @@ int binarysearch(int arr[], int n, int search)
 }
 int main()
 {
-    int n,i, search;
+    int n, i, search;
     printf("Enter number of element :");
     scanf("%d", &n);
     int arr[n];
@@ -30,20 +30,20 @@ int main()
     printf("Aray element:");
     for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
     printf("Enetr the element to Search:");
     scanf("%d", &search);
 
     int result = binarysearch(arr, n, search);
 
-    if (result !=-1)
+    if (result != -1)
     {
         printf("Element %d found %d position\n", search, result + 1);
     }
     else
     {
-        printf(" element %d not found in this array\n" ,search);
+        printf(" element %d not found in this array\n", search);
     }
     return 0;
 }
