fb2d611ac5fa900060f857121d35e8e211f3c20b
Sajedin Siam <sajedinsiam73@gmail.com>
2026-03-11 05:39:48 +0600

    function prototype

---
+++ function1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+// declearaton/prototype
+void printhello();
+int main()
+{
+    //function call
+    printhello();
+    printhello();
+    return 0;
+}
+// function definition
+void printhello()
+{
+    printf("Hello\n");
+}
