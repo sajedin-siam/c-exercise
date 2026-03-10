a6c34e5cd48c80f2aeaffef812c0454bed4e55b0
Sajedin Siam <sajedinsiam73@gmail.com>
2026-03-11 05:41:28 +0600

    print good bye

---
+++ function2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printhello();
+void printgoodbye();
+
+int main(){
+    printhello();
+    printgoodbye();
+    return 0;
+}
+void printhello(){
+    printf("Hello\n");
+}
+void printgoodbye(){
+    printf("Good Bye\n");
+}
