88b81fb91086590d196e80c69e0e700e9c6fb6a0
Siam <sajedinsiam73@gmail.com>
2026-08-12 09:22:00 +0600

    Add linear search implementation

--- LAB Assignment/linearSearch.c
+++ LAB Assignment/linearSearch.c
@@ -11,18 +11,25 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
+
     int target;
     printf("Enter target element: ");
     scanf("%d",&target);
-
+int found=0;
     for (int i = 0; i < n; i++)
     {
         if (arr[i]==target)
         {
-          printf("Trageted array index :%d ",i);
+          printf("Trageted element array index :%d \n",i);
+          found=1;
         }
         
     }
+    if (found==0)
+    {
+       printf("Element not found");
+    }
+    
     printf("\n");
     return 0;
 }
